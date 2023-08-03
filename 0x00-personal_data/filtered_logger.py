a38558adca4200fe149ebc9b6ab15af0f31a82b7
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-03 20:55:09 +0100

    complete second task

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,3 +13,24 @@ def filter_datum(fields: List[str], redaction: str,
         tmp = re.sub(field + "=.*?" + separator,
                      field + "=" + redaction + separator, tmp)
     return tmp
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize class"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Format log output"""
+        return filter_datum(self.fields,
+                            self.REDACTION, super(
+                                RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
