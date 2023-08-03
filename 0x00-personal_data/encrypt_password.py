6d9f69569c94119817de1f0a98ab7652632a290f
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-03 22:08:32 +0100

    checks if password is correct`

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,11 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """return a hashed password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hash_password: bytes, password: str) -> bool:
+    """Checks if a user password is correct"""
+    if bcrypt.checkpw(password.encode('utf-8'), hash_password):
+        return True
+    else:
+        return False
