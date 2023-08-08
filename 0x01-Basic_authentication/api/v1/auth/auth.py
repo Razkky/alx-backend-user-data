ee357235a5d671236444568400c1bc62f0f6fbfc
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-08 09:41:14 +0100

    Define authorization class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""This module contains the user authentication mechanism"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Authentication class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """This methods return False"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Returns None"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """This methods return None"""
+        return None
