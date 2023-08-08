6370f6e66e443086b4daebc5fff958219a4213b9
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-08 11:34:57 +0100

    Define the BasicAuth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/evn python3
+"""This module contains the basic_auth class"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAucth class that holds all methods for basic authentication"""
+    pass
