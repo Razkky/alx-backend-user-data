dd9f9d823eafa045095b767ca968c0c00dabf5d0
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-09 09:59:58 +0100

    a fucntion that decodes b64 header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """This module contains the basic_auth class"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header.split('Basic ')[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Return decoded value of base64 authorization_header"""
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = b64decode(base64_authorization_header).decode('utf-8')
+            return decoded
+        except Exception:
+            return None
