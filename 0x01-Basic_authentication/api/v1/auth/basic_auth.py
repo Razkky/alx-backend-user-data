a730c3ca8c6827670da586b6bda1f9ef744c9cc9
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-09 10:47:12 +0100

    retrieve user using user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """This module contains the basic_auth class"""
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -42,7 +44,24 @@ class BasicAuth(Auth):
         if not isinstance(decoded_base64_authorization_header, str):
             return None, None
         if ':' not in decoded_base64_authorization_header:
+
             return None, None
         else:
             email, password = decoded_base64_authorization_header.split(':')
             return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Return User instance based on user email and password"""
+        if not user_email and not isinstance(user_email, str):
+            return None
+        if not user_pwd and not isinstance(user_pwd, str):
+            return None
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        else:
+            return None
