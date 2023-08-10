6f0f7c6fdd57fb8fdcf26a23053e4f91fe73705f
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-10 13:34:09 +0100

    creates a session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,8 +3,19 @@
     that implements its authentication mechanism
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Implements the session authentication mechanism"""
-    pass
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for a user"""
+        if not user_id or not isinstance(user_id, str):
+            return None
+        else:
+            id = str(uuid4())
+            SessionAuth.user_id_by_session_id[id] = user_id
+            return id
