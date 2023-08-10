14a53f6020e03d8fc057a7f7063771c37933ef08
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-10 13:18:45 +0100

    fix pycodestyle

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-"""Conatins the SessionAuth class that implements its authentication mechanism"""
+"""Conatins the SessionAuth class
+    that implements its authentication mechanism
+"""
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Implements the session authentication mechanism"""
     pass
-    
