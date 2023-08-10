af7bc013a179633a04b26c00c8d85329b7027438
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-10 12:40:49 +0100

    set the value of request.current_user to the current user

--- 0x02-Session_authentication/v1/app.py
+++ 0x02-Session_authentication/v1/app.py
@@ -31,6 +31,7 @@ def before_request() -> None:
                 abort(401)
             if not auth.current_user(request):
                 abort(403)
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(401)
