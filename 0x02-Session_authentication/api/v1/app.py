1236abe7425946153ddd2b65fccafa496e1e87ef
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-10 17:04:29 +0100

    fix typo error

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-if os.getenv('AUTH_TYP') == 'session_auth':
+if os.getenv('AUTH_TYPE') == 'session_auth':
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 if os.getenv('AUTH_TYPE') == 'basic_auth':
