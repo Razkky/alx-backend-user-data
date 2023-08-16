27896703ea23df211a79914b708456454a721c25
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-08-16 14:32:31 +0100

    Make use of DB methods instead

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -20,11 +21,9 @@ class Auth:
 
     def register_user(self, email: str, password: str) -> User:
         """Register a user to the database"""
-        if self._db._session.query(User).filter_by(email=email).first():
+        try:
+            self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
-        else:
-            hashed_psw = _hash_password(password)
-            user = User(email=email, hashed_password=hashed_psw)
-            self._db._session.add(user)
-            self._db._session.commit()
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
             return user
